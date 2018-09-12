c4dcbb6721da69cf7b47c784a370d0ee721dcc3c
Sixtus Nnamdi <sixtus.nnamdi@gmail.com>
2018-09-12 11:11:31 +0100

    the best way to know if the new contact was added to the Address Book

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,5 +1,11 @@
 describe('Address Book', function() {
 	it('should be able to add a contact', function() {
+		var addressBook = new AddressBook(),
+			thisContact = new Contact();
 
+		addressBook.addContact(thisContact);
+
+		//the best way to know if the new contact was added to the Address Book
+		expect(addressBook.getContact(0)).toBe(thisContact);
 	});
 });
