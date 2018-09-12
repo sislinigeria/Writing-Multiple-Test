db1f49028c6b89e93a7d86d057b1cf8b661fe67c
Sixtus Nnamdi <sixtus.nnamdi@gmail.com>
2018-09-12 11:44:27 +0100

    set delete feature spec

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -8,4 +8,11 @@ describe('Address Book', function() {
 		//the best way to know if the new contact was added to the Address Book
 		expect(addressBook.getContact(0)).toBe(thisContact);
 	});
+
+	it('should be able to delete a contact', function() {
+		var addressBook = new AddressBook(), thisContact = new Contact();
+
+		addressBook.addContact(thisContact);
+		addressBook.deleteContact(0);
+	})
 });
